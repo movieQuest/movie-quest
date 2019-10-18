de2306f770bd643b3c11d1eacc0dbd55a0e0d4a1
dscs4thakkar <dscs4thakkar@yahoo.com>
2019-10-18 14:18:06 -0500

    actors.js file added to models folder

---
+++ models/actors.js
@@ -0,0 +1,30 @@
+
+module.exports = function(sequelize, DataTypes) {
+    var Actors = sequelize.define("Actors", {
+      // Giving the Author model a name of type STRING
+      Actor_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [2-30]
+        }
+      },
+      Gender: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        len: [4-6]
+      }
+    });
+  
+    Actors.associate = function(models) {
+      // Associating Author with Posts
+      // When an Author is deleted, also delete any associated Posts
+      Actors.hasMany(models.Movies, {
+        onDelete: "cascade"
+      });
+    };
+  
+    return Actors;
+  };
+
+  
