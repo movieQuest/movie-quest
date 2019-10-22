c7f438c04cf35a8b1a187d34e60de002841193c5
dscs4thakkar <dscs4thakkar@yahoo.com>
2019-10-22 14:04:11 -0500

    survey.html page updated

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,87 +1,82 @@
 
 
-      // Initial array of movies
-      //var movies = "";
+// displayMovieInfo function re-renders the HTML to display the appropriate content
+$("#searchBtn").on("click", function (event) {
+  event.preventDefault();
+  
+
+  $('#myModal').modal('toggle')
+
+  var movie = $("#search").val();
+  console.log(movie);
+  var queryURLPre = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+  queryURL = "";
+  for (var i = 0; i < queryURLPre.length; i++) {
+    if (queryURLPre[i] == " ") {
+      queryURL += "+"
+    } else {
+      queryURL += queryURLPre[i];
+
+    }
+
+  }
+  $("#search").val("");
+  console.log(queryURL);
 
 
 
-      // displayMovieInfo function re-renders the HTML to display the appropriate content
-      $("#searchBtn").on("click", function(event){
-        event.preventDefault();
-        // $('#myInput').trigger('focus')
-
-        $('#myModal').modal('toggle')
-        
-        var movie = $("#search").val();
-        console.log(movie);
-        var queryURLPre = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
-        queryURL = "";
-        for (var i =0; i < queryURLPre.length; i++){
-          if(queryURLPre[i] == " "){
-            queryURL += "+"
-          }else{
-            queryURL += queryURLPre[i];
-          
-          }
-
-        }
-        $("#search").val("");
-        console.log(queryURL);
-
-
-        
 
 
 
-        // Creating an AJAX call for the specific movie button being clicked
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-console.log(response);
-          // Creating a div to hold the movie
-          var movieDiv = $("<div class='movie'>");
+  // Creating an AJAX call for the specific movie button being clicked
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    // Creating a div to hold the movie
+    var movieDiv = $("<div class='movie'>");
 
-          // Storing the rating data
-          var rating = response.Rated;
+    // Storing the rating data
+    var rating = response.Rated;
 
-          // Creating an element to have the rating displayed
-          var pOne = $("<p>").text("Rating: " + rating);
+    // Creating an element to have the rating displayed
+    var pOne = $("<p>").text("Rating: " + rating);
 
-          // Displaying the rating
-          movieDiv.append(pOne);
+    // Displaying the rating
+    movieDiv.append(pOne);
 
-          // Storing the release year
-          var released = response.Released;
+    // Storing the release year
+    var released = response.Released;
 
-          // Creating an element to hold the release year
-          var pTwo = $("<p>").text("Released: " + released);
+    // Creating an element to hold the release year
+    var pTwo = $("<p>").text("Released: " + released);
 
-          // Displaying the release year
-          movieDiv.append(pTwo);
+    // Displaying the release year
+    movieDiv.append(pTwo);
 
-          // Storing the plot
-          var plot = response.Plot;
+    // Storing the plot
+    var plot = response.Plot;
 
-          // Creating an element to hold the plot
-          var pThree = $("<p>").text("Plot: " + plot);
+    // Creating an element to hold the plot
+    var pThree = $("<p>").text("Plot: " + plot);
 
-          // Appending the plot
-          movieDiv.append(pThree);
+    // Appending the plot
+    movieDiv.append(pThree);
 
-          // Retrieving the URL for the image
-          var imgURL = response.Poster;
+    // Retrieving the URL for the image
+    var imgURL = response.Poster;
 
-          // Creating an element to hold the image
-          var image = $("<img>").attr("src", imgURL);
+    // Creating an element to hold the image
+    var image = $("<img>").attr("src", imgURL);
 
-          // Appending the image
-          movieDiv.append(image);
-        $(".modal-body").html(movieDiv)
-       
-        });
-        
+    // Appending the image
+    movieDiv.append(image);
+    $(".modal-body").html(movieDiv)
+
+  });
+
 });
-      
 
-      
+
+
